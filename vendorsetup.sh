1c22bf2332c427086f191d7433cc156368059010
Juan garcia <superatmel@gmail.com>
2014-09-03 13:39:54 +0200

    Update vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-11.0 | awk {'print $1'})
-do
-    add_lunch_combo $combo
-done
+#for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-11.0 | awk {'print $1'})
+#do
+#    add_lunch_combo $combo
+#done
+
+add_lunch_combo cm_sirius-userdebug cm-11.0
