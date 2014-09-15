d96ad3f5ebae6448fb327ee67e771b4dbef05369
superatmel <superatmel@gmail.com>
2014-09-15 22:16:39 +0200

    Update personalizar.sh
    Change-Id: Ia548eaacb8d8d35b65ece7109abb80c683147d39

--- mods/terminales/sirius/personalizar.sh
+++ mods/terminales/sirius/personalizar.sh
@@ -1,4 +1,4 @@
 . mensajes.sh
 msgList2 "CON PERSONALIZACIÓN EXCLUSIVA PARA" "XPERIA Z2"
-# rm -rf $OUT/repack.d/ota/system/app/Development.apk
+rm -rf $OUT/repack.d/ota/system/app/Calendar.apk
 # rm -rf $OUT/repack.d/ota/system/app/Music.apk
