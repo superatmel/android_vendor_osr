67bc08f6f50fe4306eb33898d14627d9e201fadc
nelo360 <nelo360@gmail.com>
2014-11-25 19:47:00 +0100

    update supported devices

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,5 +8,9 @@ else
    echo " Propetarios descargados anteriormente"
 fi
 
+add_lunch_combo osr_bacon-userdebug
+add_lunch_combo osr_d802-userdebug
+add_lunch_combo osr_grouper-userdebug
 add_lunch_combo osr_jflte-userdebug
+add_lunch_combo osr_klte-userdebug
 add_lunch_combo osr_sirius-userdebug
